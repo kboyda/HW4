d893c011e983853c1c372b724eb3db1e376905f9
Kyle Boyda <kyle-boyda@uiowa.edu>
2014-09-30 20:21:47 -0500

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Net Pay Estimator<br />
+        <br />
+        Wage (Hourly):<br />
+        <asp:TextBox ID="tbWage" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Hours Worked:<br />
+        <asp:TextBox ID="tbHours" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Pre-Tax Deductions:<br />
+        <asp:TextBox ID="tbPreTax" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        After Tax Deductions:<br />
+        <asp:TextBox ID="tbAfterTax" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lbNetPay" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnClear" runat="server" Text="Clear" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,78 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        'if wage textbox is blank
+        If tbWage.Text = "" Then
+            MsgBox("Please input your hourly wage.", MsgBoxStyle.Exclamation, "Input Hourly Wage")
+        End If
+
+        'if hours text box is blank
+        If tbHours.Text = "" Then
+            MsgBox("Please input the number of hours you worked.", MsgBoxStyle.Exclamation,
+                   "Input Hours Worked")
+        End If
+
+        'if pretax deductions textbox is blank
+        If tbPreTax.Text = "" Then
+            tbPreTax.Text = "0"
+        End If
+
+        'if aftertax deductions textbox is blank
+        If tbAfterTax.Text = "" Then
+            tbAfterTax.Text = "0"
+        End If
+
+        'Declare Variables and hold values entered 
+        Dim wage As Decimal = tbWage.Text
+        Dim hours As Decimal = tbHours.Text
+        Dim preTax As Decimal = tbPreTax.Text
+        Dim afterTax As Decimal = tbAfterTax.Text
+
+
+        'Total before tax and deductions
+        Dim total As Decimal
+        total = wage * hours
+
+        'Total before tax with deductions
+        Dim total2 As Decimal
+        total2 = total - preTax
+
+        'Define tax ranges
+        Dim tax As Decimal
+        If total2 < 500 Then
+            tax = 0.18
+        End If
+
+        If total2 >= 500 Then
+            tax = 0.22
+        End If
+
+        'Total after tax
+        Dim total3 As Decimal
+        total3 = total2 - (total2 * tax)
+
+        'Net pay formula
+        Dim netPay As Decimal
+        netPay = total3 - afterTax
+        lbNetPay.Text = "Your estimated net pay is $" & netPay.ToString("#,###.##")
+
+    End Sub
+
+
+    Protected Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'clear all textbox contents
+        tbWage.Text = ""
+        tbHours.Text = ""
+        tbPreTax.Text = ""
+        tbAfterTax.Text = ""
+
+        'Clear net pay label
+        lbNetPay.Text = ""
+
+    End Sub
+
+End Class
